9ba54d437032ebebb29e1d0959f972637ee5800b
Anna Hallenberg <athallenberg@gmail.com>
2021-10-11 08:59:14 +0000

    all ekstra stuff added

--- src/compiler/semant/semenv.ml
+++ src/compiler/semant/semenv.ml
@@ -10,7 +10,54 @@ module Ty = Types
 type enventry 
   = VarEntry of Types.ty 
   | FunEntry of { formals: Types.ty list; result: Types.ty }
+  (* | NoEntry *)
 
 
 
-let baseVenv = S.empty 
+
+(* Enter the std libary somehow *)
+(* let nextVenv = S.enter( baseVenv, (S.symbol "print"), FunEntry {formals=[STRING];result=VOID}) *)
+
+let functions = 
+[(S.symbol "print");
+(S.symbol "ord");
+(S.symbol "getchar");
+(S.symbol "flush");
+(S.symbol "chr");
+(S.symbol "size");
+(S.symbol "substring");
+(S.symbol "concat");
+(S.symbol "not");
+(S.symbol "exit")]
+
+let entries = [FunEntry {formals=[STRING];result=VOID};             (*print*)
+FunEntry{formals=[STRING] ; result=INT };            (*ord*)
+  FunEntry{formals=[]; result=STRING };                (*getchar*)
+  FunEntry{formals=[]; result=VOID };                  (*flush*)
+  FunEntry{formals=[INT] ; result=STRING };            (*chr*)
+  FunEntry{formals=[STRING] ; result= INT};            (*size*)
+  FunEntry{formals=[STRING;INT;INT] ; result= STRING}; (*subString*)
+  FunEntry{formals=[STRING;STRING] ; result= STRING};  (*concat*)
+  FunEntry{formals=[INT] ; result= INT};               (*not*)
+  FunEntry{formals=[INT] ; result= VOID}               (*exit*)
+]
+
+let add env a b = S.enter(env,a,b)
+(* let baseVenv2 = List.fold_left2 add S.empty functions entries *)
+
+let baseTenv = List.fold_left2 add S.empty [S.symbol "int"; S.symbol "string"; S.symbol "void"] [Ty.INT;STRING;VOID]
+
+
+let baseVenv =  List.fold_left2 add S.empty functions entries 
+(* symbol "print" FunEntry{[STRING];VOID} *)
+(* symbol "getchar" FunEntry{[];STRING} *)
+(* symbol "ord" FunEntry{[STRING]; INT} *)
+(* symbol "substring" FunEntry{[STRING; INT; INT];STRING} *)
+(* symbol "" FunEntry{[];} *)
+
+(* let enter (venv,symbol, enventry)  = S.enter (venv, symbol, enventry) 
+
+let lookUp (venv, symbol) = S.look (venv,symbol)
+
+let size venv = S.numItems venv *)
+
--- src/compiler/semant/semenv.mli
+++ src/compiler/semant/semenv.mli
@@ -6,6 +6,8 @@ open Tigercommon
 type enventry 
   = VarEntry of Types.ty 
   | FunEntry of { formals: Types.ty list; result: Types.ty }
+  (* | NoEntry *)
 
 val baseVenv: enventry Tigercommon.Symbol.table 
+val baseTenv: Types.ty Tigercommon.Symbol.table
 
